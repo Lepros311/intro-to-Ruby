9f61965f00993e940e6d088d40fe06cbaf965f5f
Andrew S <andy.strickland@gmail.com>
2022-12-17 21:11:11 -0500

    Add exercises 2-5

---
+++ Chapter_1/Variables/Exercise_5.rb
@@ -0,0 +1,2 @@
+# The first program will print 3.
+# The second program will give an error because x is declared inside the block. It is not accessible outside of the block and so it will not print.
